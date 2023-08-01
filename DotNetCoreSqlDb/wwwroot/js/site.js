235fcd4b885c920ea2d2e9e596a3fc11e9eac366
Style90 <hermann.fopa88@gmail.com>
2023-08-01 09:38:33 +0100

    ajout du js pour le controle de la valeur du champ descriptio,

--- DotNetCoreSqlDb/wwwroot/js/site.js
+++ DotNetCoreSqlDb/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function check_desc(){
+	
+    var description = document.getElementById("desc");
+    if(description.value.trim()== ""){
+        alert("La description ne doit pas être vide");
+    }
+}
